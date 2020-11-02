b9e9ccce5139282bee65c95726d637fe91968b45
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-11-02 19:23:08 +0300

    Add params

--- src/user-api/user-controller/user-controller.controller.ts
+++ src/user-api/user-controller/user-controller.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-@Controller('users')
+@Controller('user')
 export class UserControllerController {
-  @Get()
-  getUsers(): string {
-    return 'This action returns all cats';
+  @Get(':id')
+  getUser(@Param() { id }): any {
+    return {
+      id,
+      name: 'test',
+    };
   }
 }
