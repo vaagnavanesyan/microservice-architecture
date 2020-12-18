af23a0430752e18f924a3e99c2e579505932768a
Vaagn Avanesyan <vahagn1993@gmail.com>
2020-12-18 23:37:49 +0300

    Fix install script

--- scripts/Recreate-Cluster.ps1
+++ scripts/Recreate-Cluster.ps1
@@ -4,8 +4,8 @@ minikube config set driver hyperv
 minikube start
 kubectl create ns monitoring
 kubectl config set-context --current --namespace=monitoring
-helm install prom prometheus-community/kube-prometheus-stack -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/prometheus.yaml") --atomic
-helm install nginx ingress-nginx/ingress-nginx --atomic -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/nginx-ingress.yaml") --atomic
+helm install prom prometheus-community/kube-prometheus-stack  --atomic -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/prometheus.yaml")
+helm install nginx ingress-nginx/ingress-nginx --atomic -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/nginx-ingress.yaml")
 
 kubectl create ns otus
 kubectl config set-context --current --namespace=otus
