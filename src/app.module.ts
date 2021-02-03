87a0105a3416ece80c6d66f07c66ae7752abde3e
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-02-03 21:51:46 +0300

    Add static module for frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
-
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
@@ -10,6 +11,10 @@ import { User, UserApiModule } from './user-api';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      exclude: ['/api*'],
+      rootPath: join(__dirname, '..', 'client'),
+    }),
     PrometheusModule.register({
       defaultMetrics: {
         enabled: true,
