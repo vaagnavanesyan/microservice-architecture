9e200320555a240a583d3b1820e5cd90eb0c9adc
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-01 21:59:30 +0300

    Remove static from identity

--- identity/src/app.module.ts
+++ identity/src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrometheusModule } from '@willsoto/nestjs-prometheus';
-import { join } from 'path';
 import { IdentityModule } from './identity/identity.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({
-      exclude: ['/api*'],
-      rootPath: join(__dirname, '..', 'client'),
-    }),
     PrometheusModule.register({
       defaultMetrics: {
         enabled: true,
