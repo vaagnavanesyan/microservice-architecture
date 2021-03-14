99b38de6604ca4142917e6fad420ec6cf2bdb428
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-15 01:02:49 +0300

    Route SPA to /portal with nginx

--- face-systems-portal/src/index.tsx
+++ face-systems-portal/src/index.tsx
@@ -1,13 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+
+
+  Link, Route, Switch
+} from "react-router-dom";
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <App/>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Router basename="portal">
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
