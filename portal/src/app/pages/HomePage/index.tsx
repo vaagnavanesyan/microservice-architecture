fcdbd1c816bd2a843255e9ae2c138c31545a30a2
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-16 21:54:28 +0300

    Edit HomePage and 404

--- portal/src/app/pages/HomePage/index.tsx
+++ portal/src/app/pages/HomePage/index.tsx
@@ -2,7 +2,7 @@ import { NavBar } from 'app/components/NavBar';
 import { PageWrapper } from 'app/components/PageWrapper';
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Features } from './Features';
+import { ThemeSwitch } from './Features/ThemeSwitch';
 import { Masthead } from './Masthead';
 
 export function HomePage() {
@@ -10,15 +10,12 @@ export function HomePage() {
     <>
       <Helmet>
         <title>Home Page</title>
-        <meta
-          name="description"
-          content="A React Boilerplate application homepage"
-        />
+        <meta name="description" content="FaceSystems Portal homepage" />
       </Helmet>
       <NavBar />
       <PageWrapper>
         <Masthead />
-        <Features />
+        <ThemeSwitch />
       </PageWrapper>
     </>
   );
