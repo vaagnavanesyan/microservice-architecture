e57523a485bb008177ed851013a57fb995300c00
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-24 00:23:19 +0300

    [orders] Add sqlite for local testing

--- orders/src/app.module.ts
+++ orders/src/app.module.ts
@@ -1,12 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { CommandHandlers } from './commands/handlers';
 import { EventHandlers } from './events/handlers';
 import { UserRepository } from './repositories/user.repository';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CqrsModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'sqlite',
+        database: configService.get('DATABASE_URI'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [UserRepository, ...CommandHandlers, ...EventHandlers],
 })
