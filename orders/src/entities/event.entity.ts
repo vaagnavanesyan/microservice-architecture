35878666d38a3467ab0b2360f2c56abc0694adcb
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-24 16:26:51 +0300

    [orders] save event to event store

---
+++ orders/src/entities/event.entity.ts
@@ -0,0 +1,8 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Event extends BaseEntity {
+  @PrimaryGeneratedColumn() id: number;
+  @Column() eventType: string;
+  @Column() Json: string;
+}
--- orders/src/events/handlers/order-created.handler.ts
+++ orders/src/events/handlers/order-created.handler.ts
@@ -1,12 +1,20 @@
 import { IEventHandler } from '@nestjs/cqrs';
 import { EventsHandler } from '@nestjs/cqrs/dist/decorators/events-handler.decorator';
+import { Event } from 'src/entities/event.entity';
+import { nameof } from 'ts-simple-nameof';
+import { getRepository } from 'typeorm';
 import { OrderCreatedEvent } from '../impl/order-created.event';
 
 @EventsHandler(OrderCreatedEvent)
 export class OrderCreatedEventHandler
   implements IEventHandler<OrderCreatedEvent> {
-  handle(event: OrderCreatedEvent) {
+  async handle(event: OrderCreatedEvent) {
     console.log('OrderCreatedEvent...');
-    console.log(event);
+    const repo = getRepository(Event);
+    const record = repo.create({
+      eventType: nameof(OrderCreatedEvent),
+      Json: JSON.stringify(event),
+    });
+    await record.save();
   }
 }
