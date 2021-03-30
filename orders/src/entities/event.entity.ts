bfa2db5e4ea860642e4899167c5c4d150d415405
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-03-30 12:13:44 +0300

    [orders] Add Date column to Event

--- orders/src/entities/event.entity.ts
+++ orders/src/entities/event.entity.ts
@@ -3,6 +3,7 @@ import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Event extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
+  @Column() date: Date = new Date();
   @Column() eventType: string;
   @Column() json: string;
 }
