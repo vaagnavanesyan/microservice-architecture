ffcd51f93ff89b09818ab58b68d3a88dcdd46bed
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-03 16:46:24 +0300

    [orders] Fix published event type

--- orders/src/orders/events/order-price-changed.handler.ts
+++ orders/src/orders/events/order-price-changed.handler.ts
@@ -2,10 +2,7 @@ import { Inject } from '@nestjs/common';
 import { IEventHandler } from '@nestjs/cqrs';
 import { EventsHandler } from '@nestjs/cqrs/dist/decorators/events-handler.decorator';
 import { ClientProxy } from '@nestjs/microservices';
-import {
-  OrderCreatedEvent,
-  OrderPriceChangedEvent,
-} from '@vaagnavanesyan/common';
+import { OrderPriceChangedEvent } from '@vaagnavanesyan/common';
 import { Queues } from 'src/orders/constants';
 import { Event } from 'src/orders/entities/event.entity';
 import { nameof } from 'ts-simple-nameof';
@@ -26,6 +23,6 @@ export class OrderPriceChangedHandler
     });
 
     await record.save();
-    this.ordersQueue.emit(nameof(OrderCreatedEvent), payload);
+    this.ordersQueue.emit(nameof(OrderPriceChangedEvent), payload);
   }
 }
