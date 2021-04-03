1396e920744fb30013bf6cf3f70275f33fb46251
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-03 18:42:34 +0300

    [common] Remove uncommon events

--- common/src/events/index.ts
+++ common/src/events/index.ts
@@ -1,6 +1,4 @@
 export { CheckoutOrderEvent } from './checkout-order.event';
-export { OrderCreatedEvent } from './order-created.event';
 export { OrderPaidEvent } from './order-paid.event';
-export { OrderPriceChangedEvent } from './order-price-changed.event';
 export { PaymentRefusedEvent } from './payment-refused.event';
 export { UserCreatedEvent } from './user-created.event';
--- common/src/payloads/index.ts
+++ common/src/payloads/index.ts
@@ -1,9 +1,4 @@
-export { AddImagePayload } from './add-image.payload';
 export { CheckoutOrderPayload } from './checkout-order.payload';
-export { CreateOrderPayload } from './create-order-payload';
-export { OrderCreatedPayload } from './order-created-payload';
 export { OrderPaidPayload } from './order-paid.payload';
-export { OrderPriceChangedPayload } from './order-price-changed.payload';
 export { PaymentRefusedPayload } from './payment-refused.payload';
-export { RemoveImagePayload } from './remove-image.payload';
 export { UserCreatedPayload } from './user-created.payload';
