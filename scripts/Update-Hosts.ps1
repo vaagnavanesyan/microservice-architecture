800405af8f2b8913f7bae56677749d2101326b20
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-06 23:32:29 +0300

    remove profile arg

--- scripts/Update-Hosts.ps1
+++ scripts/Update-Hosts.ps1
@@ -1,5 +1,5 @@
 Import-Module 'Carbon'
-$minikubeIP = minikube ip -p otus
+$minikubeIP = minikube ip
 $hostname = "arch.homework"
 Remove-HostsEntry -HostName $hostname
 Set-HostsEntry -IPAddress $minikubeIP -HostName $hostname -Description "Host for OTUS labs"
