6acb516a196d081f341e1ac36502a01b91d86057
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-14 22:07:16 +0300

    [scripts] Add script to setup infrastructure dependencies

---
+++ scripts/Setup-Infra.ps1
@@ -0,0 +1,9 @@
+Write-Host 🔥 Installing Prometheus
+kubectl create ns monitoring
+helm install prom prometheus-community/kube-prometheus-stack  --atomic -n monitoring -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/prometheus.yaml")
+
+Write-Host 🆖 Installing Nginx
+helm install nginx ingress-nginx/ingress-nginx --atomic -f $(Join-Path -Path $PSScriptRoot -ChildPath "./configs/nginx-ingress.yaml")
+
+Write-Host 🐇 Installing RabbitMQ...
+helm install rabbitmq bitnami/rabbitmq --set auth.username=user --set auth.password=bX1DTrlOfH -n otus
