1600ddbb86da149e7b01a65ed0e13be126aedb6b
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-15 09:58:41 +0300

    [billing] Create user account

--- billing-worker/src/app.module.ts
+++ billing-worker/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BillingController } from './controllers/billing.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URI'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [BillingController],
 })
 export class AppModule {}
--- billing-worker/src/controllers/billing.controller.ts
+++ billing-worker/src/controllers/billing.controller.ts
@@ -1,12 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
-import { UserCreatedEvent } from '@vaagnavanesyan/common';
+import { UserCreatedEvent, UserCreatedPayload } from '@vaagnavanesyan/common';
+import { User } from 'src/entities/user.entity';
 import { nameof } from 'ts-simple-nameof';
+import { getRepository } from 'typeorm';
 
 @Controller()
 export class BillingController {
   @EventPattern(nameof(UserCreatedEvent))
-  async handleUserCreated(data: Record<string, unknown>) {
-    console.log(data);
+  async handleUserCreated(data: UserCreatedPayload) {
+    const repo = getRepository(User);
+    await repo.save(data);
   }
 }
