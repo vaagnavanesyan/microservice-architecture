c8a0298d73c2db91885fa2d0573e5f6e0c397932
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-04-19 20:12:43 +0300

    [orders] Remove unused handlers

--- orders-api/src/orders/events/handlers/index.ts
+++ orders-api/src/orders/events/handlers/index.ts
@@ -1,15 +1,11 @@
 import { CheckoutOrderEventHandler } from './checkout-order-event-handler';
 import { OrderCancelledHandler } from './order-cancelled.handler';
 import { OrderCreatedEventHandler } from './order-created.handler';
-import { OrderPaidHandler } from './order-paid.handler';
 import { OrderPriceChangedHandler } from './order-price-changed.handler';
-import { PaymentRefusedHandler } from './payment-refused.handler';
 
 export const EventHandlers = [
   CheckoutOrderEventHandler,
   OrderCreatedEventHandler,
   OrderCancelledHandler,
-  OrderPaidHandler,
   OrderPriceChangedHandler,
-  PaymentRefusedHandler,
 ];
