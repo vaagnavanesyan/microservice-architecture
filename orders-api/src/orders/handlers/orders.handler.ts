08ab8f6877e654daa31beebe75155f3bb3b8ec73
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-05-10 00:10:04 +0300

    [orders] Publish event when order is ready to be processed

--- orders-api/src/orders/handlers/orders.handler.ts
+++ orders-api/src/orders/handlers/orders.handler.ts
@@ -1,6 +1,8 @@
 import { AmqpConnection, RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { Injectable } from '@nestjs/common';
 import {
+  OrderReadyToProcessEvent,
+  OrderReadyToProcessPayload,
   PaymentProceedEvent,
   PaymentProceedPayload,
   PaymentRefusedEvent,
@@ -10,7 +12,7 @@ import {
 } from '@vaagnavanesyan/common';
 import { nameof } from 'ts-simple-nameof';
 import { getRepository } from 'typeorm';
-import { Order } from '../entities';
+import { Image, Order } from '../entities';
 import { OrderStatuses } from '../enums/order-statuses.enum';
 
 @Injectable()
@@ -37,5 +39,12 @@ export class OrdersHandler {
     const order = await getRepository(Order).findOne(data.orderId);
     order.status = OrderStatuses.PaymentSucceeded;
     await order.save();
+
+    const images = await getRepository(Image).find({ where: { order }, select: ['objectPath'] });
+    const payload: OrderReadyToProcessPayload = {
+      imageObjectNames: images.map((e) => e.objectPath),
+      orderId: order.id,
+    };
+    this.queue.publish(RabbitMQDirectExchange, nameof(OrderReadyToProcessEvent), payload);
   }
 }
