ab31d3b6e7b469810d21e78f2d8a9f10947144f4
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-05-10 14:17:10 +0300

    [images] Highlight faces on image

--- image-processor/Program.cs
+++ image-processor/Program.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
 
 namespace ImageProcessor
 {
@@ -14,15 +14,13 @@ namespace ImageProcessor
             var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cascades", "haarcascade_frontalface_default.xml");
             CascadeClassifier faceCascade = new CascadeClassifier();
             faceCascade.Load(file);
-            var faceRects = faceCascade.DetectMultiScale(source, 1.2, 6, HaarDetectionTypes.DoRoughSearch, new Size(64, 64));
+            var faceRects = faceCascade.DetectMultiScale(source, 1.1, 9, HaarDetectionTypes.DoRoughSearch, new Size(64, 64));
             var faces = new List<byte[]>();
             for (int i = 0; i < faceRects.Length; i++)
             {
-                var face = new Mat(source, faceRects[i]);
-                faces.Add(face.ToBytes(".jpg"));
-                face.SaveImage(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "face" + i + ".jpg"), new ImageEncodingParam(ImwriteFlags.JpegProgressive, 255));
-                Console.WriteLine($"Saved {i}");
+                source.Rectangle(faceRects[i], Scalar.Red, 4);
             }
+            source.SaveImage(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "048-2.jpg"), new ImageEncodingParam(ImwriteFlags.JpegProgressive, 255));
         }
     }
 }
