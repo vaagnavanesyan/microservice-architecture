9521391944a5c6962f92821a7412ac603827e3e3
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-05-11 01:57:09 +0300

    [images] Add basic RabbitMQ Consumer

--- image-processor/ImageProcessor/Program.cs
+++ image-processor/ImageProcessor/Program.cs
@@ -1,5 +1,7 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +21,15 @@ namespace ImageProcessor
             .ConfigureServices((hostContext, services) =>
             {
               services.AddHostedService<Worker>();
+              services.AddSingleton<OrderReadyToProcessEventConsumer>();
+              services.AddSingleton(context =>
+              {
+                var rmqSettings = hostContext.Configuration.GetSection("RabbitMQ");
+                var hostname = rmqSettings.GetValue<string>("HostName");
+                var username = rmqSettings.GetValue<string>("UserName");
+                var password = rmqSettings.GetValue<string>("Password");
+                return new ConnectionFactory() { HostName = hostname, UserName = username, Password = password };
+              });
             });
   }
 }
--- image-processor/ImageProcessor/Worker.cs
+++ image-processor/ImageProcessor/Worker.cs
@@ -14,26 +14,26 @@ namespace ImageProcessor
   {
     private readonly ILogger<Worker> _logger;
     private readonly IConfiguration _configuration;
+    private readonly OrderReadyToProcessEventConsumer _rabbitMQConsumer;
 
-    public Worker(ILogger<Worker> logger, IConfiguration configuration, IHostEnvironment env)
+    public Worker(ILogger<Worker> logger, IConfiguration configuration, IHostEnvironment env, OrderReadyToProcessEventConsumer rabbitMQConsumer)
     {
       _logger = logger;
       _configuration = configuration;
+      _rabbitMQConsumer = rabbitMQConsumer;
       if (env.IsProduction())
       {
         // read from env
       }
       else
       {
-        var rabbitmq = configuration.GetSection("RabbitMQ");
-        var password = rabbitmq.GetValue<string>("Password");
-        System.Console.WriteLine(password);
       }
 
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+      await _rabbitMQConsumer.RegisterConsumer(stoppingToken);
       using var source = new Mat("048.jpg", ImreadModes.Color);
       var file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Cascades", "haarcascade_frontalface_default.xml");
       CascadeClassifier faceCascade = new CascadeClassifier();
