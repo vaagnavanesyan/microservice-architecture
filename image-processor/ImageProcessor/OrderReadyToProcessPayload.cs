b67f64a8cbdb6f98d1d9f0bf1f07643be98f9898
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-05-15 00:59:54 +0300

    [images] Publish message when order is processed

--- image-processor/ImageProcessor/OrderReadyToProcessPayload.cs
+++ image-processor/ImageProcessor/OrderReadyToProcessPayload.cs
@@ -4,5 +4,8 @@
   {
     public int OrderId { get; set; }
     public string[] ImageObjectPaths { get; set; }
+    public string PayerEmail { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
   }
 }
