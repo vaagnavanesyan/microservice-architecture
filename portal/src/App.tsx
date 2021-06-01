5744f8f3f45ab028d5cdf0507067d3ca5a6e6ede
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-06-01 18:41:32 +0300

    [portal] Add basic layout and login component

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,13 +1,23 @@
+import { Layout } from 'antd';
 import React from 'react';
 import './App.css';
+import { Footer } from './components/footer/footer-component';
+import { Login } from './components/login/login-component';
 import { NavBar } from './components/navbar/navbar-component';
 
 const App = () => (
   <>
-    <NavBar />
-    <div className="content">
-      <h2>Your content would go here</h2>
-    </div>
+    <Layout>
+      <Layout.Header className="header">
+        <NavBar />
+      </Layout.Header>
+      <Layout.Content className="content">
+        <Login />
+      </Layout.Content>
+      <Layout.Footer>
+        <Footer />
+      </Layout.Footer>
+    </Layout>
   </>
 );
 
---
+++ portal/src/components/login/login-component.tsx
@@ -0,0 +1,36 @@
+import { Button, Checkbox, Form, Input } from 'antd';
+import './login-component.css';
+const tailLayout = {
+  wrapperCol: { offset: 12 },
+};
+export const Login = () => {
+  const onFinish = (values: any) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form className="form" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+      <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
+        <Input />
+      </Form.Item>
+
+      <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
