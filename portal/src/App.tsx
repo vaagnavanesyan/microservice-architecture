9fb8541e3c06335e7a8134db109d199c36341013
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-06-05 12:44:28 +0300

    [orders] Add router

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -1,49 +1,60 @@
-import { Button, Empty, Layout, Result } from 'antd';
+import { Layout } from 'antd';
 import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { Footer } from './components/footer/footer-component';
 import { Login } from './components/login/login-component';
 import { NavBar } from './components/navbar/navbar-component';
+import { OrdersEmptyList } from './components/orders/empty/orders-empty.component';
 import { Orders } from './components/orders/orders-component';
+import { SuccessMessage } from './components/success/success-component';
 
-const App = () => (
-  <>
-    <Layout>
-      <Layout.Header className="header">
-        <NavBar />
-      </Layout.Header>
-      <Layout.Content className="content">
-        <Login />
-        <hr />
-        <Empty
-          image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
-          imageStyle={{
-            height: 60,
-          }}
-          description={<span>You don't have orders yet</span>}
-        >
-          <Button type="primary">Make first order</Button>
-        </Empty>
-        <hr />
-        <Orders />
-        <hr />
-        <Result
-          status="success"
-          title="Successfully Purchased Cloud Server ECS!"
-          subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
-          extra={[
-            <Button type="primary" key="console">
-              Go Console
-            </Button>,
-            <Button key="buy">Buy Again</Button>,
-          ]}
-        />
-      </Layout.Content>
-      <Layout.Footer>
-        <Footer />
-      </Layout.Footer>
-    </Layout>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Layout>
+          <Layout.Header className="header">
+            <NavBar />
+          </Layout.Header>
+          <Layout.Content className="content">
+            <Switch>
+              {/* TODO: Change to real home page*/}
+              <Route exact path="/" component={SuccessMessage} />
+              <Route path="/signin" component={Login}></Route>
+              <PrivateRoute path="/orders">
+                <Orders />
+              </PrivateRoute>
+              {/* TODO: Not found page here */}
+              <Route component={OrdersEmptyList} />
+            </Switch>
+          </Layout.Content>
+          <Footer />
+        </Layout>
+      </BrowserRouter>
+    </>
+  );
+};
+
+function PrivateRoute({ children, ...rest }) {
+  let auth = true; //isAuthorized();
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/signin',
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
 
 export default App;
