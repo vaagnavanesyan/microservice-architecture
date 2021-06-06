3b7bcfa70d4bd2ec69689ad56494cec07e125494
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-06-07 00:28:32 +0300

    [portal] Show real user name and amount from API

--- portal/src/components/navbar/navbar-component.tsx
+++ portal/src/components/navbar/navbar-component.tsx
@@ -1,24 +1,33 @@
 import { DollarOutlined, FileImageOutlined, LogoutOutlined, UserOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-import faker from 'faker';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getAmount, getProfile } from '../../utils/api-requests';
 import './navbar-component.css';
 
-export const NavBar = () => (
-  <Menu theme="dark" mode="horizontal">
-    <Menu.Item key="orders" icon={<FileImageOutlined />}>
-      <a href="/orders">Orders</a>
-    </Menu.Item>
-    <Menu.SubMenu key="account" icon={<UserOutlined />} title={faker.name.firstName()}>
-      <Menu.Item key="profile" icon={<UserOutlined />}>
-        <a href="/profile">Edit profile</a>
+export const NavBar = () => {
+  const [fullName, setFullName] = useState('');
+  const [amount, setAmount] = useState(0.0);
+  useEffect(() => {
+    console.log('Get profile...');
+    getProfile().then((profile) => setFullName(`${profile.firstName} ${profile.lastName}`));
+    getAmount().then(setAmount);
+  }, []);
+  return (
+    <Menu theme="dark" mode="horizontal">
+      <Menu.Item key="orders" icon={<FileImageOutlined />}>
+        <a href="/orders">Orders</a>
       </Menu.Item>
-      <Menu.Item key="amount" icon={<DollarOutlined />}>
-        123.00
-      </Menu.Item>
-      <Menu.Item key="logout" icon={<LogoutOutlined />}>
-        <a href="/logout">Log out</a>
-      </Menu.Item>
-    </Menu.SubMenu>
-  </Menu>
-);
+      <Menu.SubMenu key="account" icon={<UserOutlined />} title={fullName}>
+        <Menu.Item key="profile" icon={<UserOutlined />}>
+          <a href="/profile">Edit profile</a>
+        </Menu.Item>
+        <Menu.Item key="amount" icon={<DollarOutlined />}>
+          {amount}
+        </Menu.Item>
+        <Menu.Item key="logout" icon={<LogoutOutlined />}>
+          <a href="/logout">Log out</a>
+        </Menu.Item>
+      </Menu.SubMenu>
+    </Menu>
+  );
+};
