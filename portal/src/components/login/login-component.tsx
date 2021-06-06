93cf30071e27e32522d6ada0b8fab850ccc358e5
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-06-07 00:28:54 +0300

    [portal] Login scenario with API

--- portal/src/components/login/login-component.tsx
+++ portal/src/components/login/login-component.tsx
@@ -1,36 +1,50 @@
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Alert, Button, Form, Input, Space, Spin } from 'antd';
+import React, { useState } from 'react';
+import { signIn } from '../../utils/api-requests';
 import './login-component.css';
 const tailLayout = {
   wrapperCol: { offset: 12 },
 };
 export const Login = () => {
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
-
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
+  const [isLoading, setLoading] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const onFinish = async (values: any) => {
+    setLoading(true);
+    const { email, password } = values;
+    const token = await signIn(email, password);
+    setLoading(false);
+    if (!token) {
+      setShowError(true);
+    }
+    console.log('token :>> ', token);
   };
 
   return (
-    <Form className="form" initialValues={{ remember: true }} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <Form.Item label="Username" name="username" rules={[{ required: true, message: 'Please input your username!' }]}>
-        <Input />
-      </Form.Item>
+    <Space direction="vertical">
+      {showError && <Alert message="Login failed" type="error" showIcon />}
+      <Form className="form" onFinish={onFinish}>
+        <Form.Item label="Email" name="email" rules={[{ required: true, message: 'Please input email!' }]}>
+          <Input />
+        </Form.Item>
 
-      <Form.Item label="Password" name="password" rules={[{ required: true, message: 'Please input your password!' }]}>
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item {...tailLayout}>
+          {isLoading ? (
+            <Spin />
+          ) : (
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          )}
+        </Form.Item>
+      </Form>
+    </Space>
   );
 };
