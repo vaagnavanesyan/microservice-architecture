cb296bcdc51e44ec329e9c5ecb9d8c4442fa8733
Vaagn Avanesyan <vahagn1993@gmail.com>
2021-06-12 06:29:46 +0300

    [portal] Add main order page with steps

--- portal/src/components/order/order-component.tsx
+++ portal/src/components/order/order-component.tsx
@@ -1,26 +1,57 @@
-import { Carousel } from 'antd';
-import React from 'react';
+import { CheckCircleOutlined, CreditCardOutlined, FileImageOutlined, SyncOutlined } from '@ant-design/icons';
+import { Steps } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-const contentStyle = {
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
-export const Order = () => (
-  <Carousel effect="fade">
-    <div>
-      <h3 style={contentStyle as any}>1</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle as any}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle as any}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle as any}>4</h3>
-    </div>
-  </Carousel>
-);
+import { getOrder } from '../../utils/api-requests';
+
+const { Step } = Steps;
+const steps = [
+  {
+    title: 'Добавление изображений',
+    icon: <FileImageOutlined />,
+    content: 'добавление',
+  },
+  {
+    title: 'Оплата',
+    icon: <CreditCardOutlined />,
+    content: 'оплата тутъ',
+  },
+  {
+    title: 'Обработка заказа',
+    icon: <SyncOutlined />,
+    content: 'обработка...',
+  }, {
+    title: 'Результат',
+    icon: <CheckCircleOutlined />,
+    content: 'результат тут',
+  },
+];
+export const Order = () => {
+  const [current, setCurrent] = React.useState(0);
+
+  const next = () => {
+    setCurrent(current + 1);
+  };
+
+  const prev = () => {
+    setCurrent(current - 1);
+  };
+
+  const { id } = useParams<{ id: string }>();
+  const [order, setOrder] = useState({});
+  useEffect(() => {
+    const orderId = parseInt(id, 10);
+    getOrder(orderId).then(setOrder);
+  }, [id]);
+  return <>
+    <Steps current={current}>
+      {steps.map(item => (
+        <Step key={item.title} title={item.title} icon={item.icon} />
+      ))}
+    </Steps>
+    <div className="steps-content">{steps[current].content}</div>
+  </>
+
+
+}
